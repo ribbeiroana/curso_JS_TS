0b05fd4bb1fbfb9a5309d65a88fa8ad221fe97e2
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-06-18 08:54:03 -0300

    aula

---
+++ Curso/aula41/index.js
@@ -0,0 +1,11 @@
+// function ePaisagem (largura, altura) { //recebe altura e largura
+// return largura > altura // ? true : false ; não é necessario porue a função já retorna true ou false 
+
+// }
+
+// console.log(ePaisagem(1920, 1080));
+
+
+const ePaisagem = (largura, altura) = {
+
+}
