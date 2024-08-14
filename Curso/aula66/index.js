e698d4bca99c93307b92a889390aedf401adcbcb
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-08-14 17:10:39 -0300

    aulas

--- Curso/aula66/index.js
+++ Curso/aula66/index.js
@@ -1,12 +1,12 @@
 // MAP
 
 // dobre os numeros 
-const num = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27 ];
-const numerosDobro = num.map(function(valor) {
-    return (2*`${valor}`);
-});
+// const num = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27 ];
+// const numerosDobro = num.map(function(valor) {
+//     return (2*`${valor}`);
+// });
+// console.log(numerosDobro);
 
-console.log(numerosDobro);
 
 // Para cada elemento
 // const pessoas = [ 
@@ -19,9 +19,20 @@ console.log(numerosDobro);
 // ];
 
 // Retorne apenas uma string com o nome da pessoa 
-const nomes = pessoas.map(obj => obj.nome.join('') )
+// const nomes = pessoas.map(obj => obj.nome)
+// console.log(nomes);
+
 
 // Remova apenas a chave 'nome' do objeto
+// const idade = pessoas.map(obj => obj.idade)
+// console.log(idade);
 
 
-// Adicione uma chave  id em cada objeto
+// Adicione uma chave id em cada objeto 
+// const comIds = pessoas.map(function(obj, indice) {
+//     const newObj = { ...obj }; // tem que criar um new obj para não afetar o array original
+//     newObj.id = indice 
+//     return  newObj;
+// })
+// console.log(comIds);
+// console.log(pessoas);
