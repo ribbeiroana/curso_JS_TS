0dc9bff86901a98a64507933da7ba3b9a32d8930
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2024-08-16 17:07:01 -0300

    aulas

--- Curso/aula73/index.js
+++ Curso/aula73/index.js
@@ -7,8 +7,25 @@
 // ... (SPREAD)
 
 
-const produto = { nome: 'Caneca', preco: 1.80};
-const outraCoisa = produto;
+// const produto = { nome: 'Caneca', preco: 1.80};
+// const outraCoisa = produto;
 
-produto.nome = "Ana";
-console.log(outraCoisa);
+// produto.nome = "Ana";
+// console.log(outraCoisa);
+
+const produto = { nome: 'Caneca', preco: 1.80};
+for (let entry of Object.entries(produto)) {
+    console.log(entry);
+}
+console.log(Object.getOwnPropertyDescriptor(produto, 'nome')) // passa os parametros (objeto e a propriedade que deseja ver)
+
+
+// Object.entries - para ver os valores e as chaves
+// resposta 
+// {
+//     value: 'Caneca',
+//     writable: true,
+//     enumerable: true,
+//     configurable: true
+//   }
+  
